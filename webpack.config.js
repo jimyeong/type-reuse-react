26a2ff171b078c133de3a885aee944a3b53967bd
jimyeong <idjjm92@gmail.com>
2019-01-30 11:37:10 +0900

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,16 @@ module.exports = {
     },
     module: {
         rules:[
-
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
             {
                 test: /\.less$/,
                 use:[
